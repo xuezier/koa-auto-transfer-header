ca90e8262d7b60f82e0a3bb742e7d220023ca2b4
tunan <tunan@gaoding.com>
2023-02-23 11:10:08 +0800

    chore: support server options

--- src/re-http-server.ts
+++ src/re-http-server.ts
@@ -4,24 +4,36 @@ import { config } from './config';
 import { storage } from './storage';
 
 const httpCreateServer = http.createServer;
-Object.defineProperty(http, 'createServer', {
-    value: (callback: http.RequestListener) => {
-        return httpCreateServer((req, res) => {
-            if(config.enable)
-                if(storage.store)
-                    return callback(req, res);
-                else
-                    storage.enable(function() {
-                        config.transferHeaders.map(headerKey => {
-                            const value = req.headers[headerKey];
-                            if(value)
-                                storage.set(headerKey, value);
-                        });
 
-                        callback(req, res);
-                    });
+const httpCallback = function(callback: http.RequestListener)  {
+    return (req: http.IncomingMessage, res: http.ServerResponse) => {
+        if(config.enable)
+            if(storage.store)
+                return callback(req, res);
             else
-                callback(req,res);
-        });
+                storage.enable(function() {
+                    config.transferHeaders.map(headerKey => {
+                        const value = req.headers[headerKey];
+                        if(value)
+                            storage.set(headerKey, value);
+                    });
+
+                    callback(req, res);
+                });
+        else
+            callback(req,res);
+    };
+};
+
+Object.defineProperty(http, 'createServer', {
+    value: (options: http.ServerOptions, callback?: http.RequestListener) => {
+
+        if(typeof options === 'function') {
+            callback = options;
+
+            return httpCreateServer(httpCallback(<any>callback));
+        }
+
+        return httpCreateServer(options, httpCallback(<any>callback));
     }
 });
--- src/re-https-server.ts
+++ src/re-https-server.ts
@@ -1,27 +1,39 @@
 import { RequestListener } from 'http';
 import * as https from 'https';
+import * as http from 'http';
 import { config } from './config';
 import { storage } from './storage';
 
 const httpsCreateServer = https.createServer;
-Object.defineProperty(https, 'createServer', {
-    value: (callback: RequestListener) => {
-        return httpsCreateServer((req, res) => {
-            if(config.enable)
+
+const httpsCallback = function(callback: http.RequestListener)  {
+    return (req: http.IncomingMessage, res: http.ServerResponse) => {
+        if(config.enable)
+            if(storage.store)
+                return callback(req, res);
+            else
                 storage.enable(function() {
-                    if(storage.store)
-                        return callback(req, res);
-                    else
-                        config.transferHeaders.map(headerKey => {
-                            const value = req.headers[headerKey];
-                            if(value)
-                                storage.set(headerKey, value);
-                        });
+                    config.transferHeaders.map(headerKey => {
+                        const value = req.headers[headerKey];
+                        if(value)
+                            storage.set(headerKey, value);
+                    });
 
                     callback(req, res);
                 });
-            else
-                callback(req,res);
-        });
+        else
+            callback(req,res);
+    };
+};
+
+Object.defineProperty(https, 'createServer', {
+    value: (options: https.ServerOptions, callback: RequestListener) => {
+        if(typeof options === 'function') {
+            callback = options;
+
+            return httpsCreateServer(httpsCallback(<any>callback));
+        }
+
+        return httpsCreateServer(options, httpsCallback(<any>callback));
     }
 });
