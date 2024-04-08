35d29ea302bcc7332c684f285340f148003a37bc
tunan <tunan@gaoding.com>
2024-04-08 15:00:58 +0800

    chore: add default transfer header

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,11 @@
 import { IConfig } from "./interface/IConfig";
 
 export const config: IConfig = {
-    transferHeaders: ['request-id', 'trace-id'],
+    transferHeaders: [
+        'request-id', 'trace-id', 'gd_request_origin',
+        'x-forwarded-for',
+        'accept-language',
+    ],
 
     enable: true,
 };
